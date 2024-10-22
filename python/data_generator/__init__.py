41a34d0244d82b786646de28cc1656cd52eab103
Ludovico Passari <ludovicopassari9@gmail.com>
2024-10-22 17:08:10 +0200

    raw dataset generation with dataset example

--- python/data_generator/__init__.py
+++ python/data_generator/__init__.py
@@ -3,8 +3,13 @@ import pandas as pd
 from faker import Faker
 import random
 
+from .generator import generate_data
+from .generator_config import event_attributes, object_attributes, person_attributes, evidence_attributes, \
+    location_attributes
+
 # Creazione dell'istanza di Faker
 fake = Faker()
 
 # Esportazione delle librerie nel namespace del pacchetto
-__all__ = ['pd', 'random', 'fake']
+__all__ = ['pd', 'random', 'fake', 'event_attributes', 'object_attributes', 'person_attributes', 'evidence_attributes',
+           'location_attributes']
