0a5561ccddec7269bfe77992bef5b2b8f86de80b
Ludovico Passari <ludovicopassari9@gmail.com>
2024-10-24 09:04:11 +0200

    person-event relationship

---
+++ python/data_generator/relationship_generator.py
@@ -0,0 +1,42 @@
+from data_generator import pd
+import numpy as np
+
+
+def person_to_event_relationship(people_entities: pd.DataFrame, event_entities: pd.DataFrame,
+                                 max_relationship=3) -> pd.DataFrame:
+    relationships = []
+
+    for index, row in people_entities.iterrows():
+        num_relationships = np.random.randint(0, max_relationship + 1)
+        related_b = np.random.choice(event_entities['id'], num_relationships, replace=False)
+
+        for b_id in related_b:
+            relationships.append({'id_person': row['id'], 'id_event': b_id})
+
+    return pd.DataFrame(relationships)
+
+
+def person_to_person(people_entities: pd.DataFrame, max_relationship=3):
+    relationships = []
+
+    for index, row in people_entities.iterrows():
+        num_relationships = np.random.randint(0, max_relationship + 1)
+        related_b = np.random.choice(people_entities['id'], num_relationships, replace=False)
+
+        for b_id in related_b:
+            relationships.append({'id_person1': row['id'], 'id_person2': b_id})
+
+    return pd.DataFrame(relationships)
+
+
+def main():
+    people = pd.read_csv(
+        '/Users/vico/Documents/projects/uni_prj/db/data-investigation-criminal-justice-report/dataset/people_data.csv')
+    events = pd.read_csv(
+        '/Users/vico/Documents/projects/uni_prj/db/data-investigation-criminal-justice-report/dataset/events_data.csv')
+
+    persons = person_to_person(people, max_relationship=3)
+    print(persons)
+
+if __name__ == '__main__':
+    main()
---
+++ python/scripts/data_generation_script.py
@@ -0,0 +1,22 @@
+from data_generator.generator_config import person_attributes, object_attributes, event_attributes, location_attributes, \
+    evidence_attributes
+from data_generator import generate_data
+from pathlib import Path
+
+# Ottenere il percorso del file Python in esecuzione
+current_file_path = Path(__file__).resolve()
+
+# Ottenere la directory corrente
+output_path = current_file_path.parent.parent.parent.joinpath('dataset')
+
+people_path = output_path.joinpath('people_data.csv')
+events_path = output_path.joinpath('events_data.csv')
+objects_path = output_path.joinpath('objects_data.csv')
+location_path = output_path.joinpath('location_data.csv')
+evidence_path = output_path.joinpath('evidence_data.csv')
+
+people = generate_data(45, person_attributes, 'csv', people_path)
+events = generate_data(30, event_attributes, 'csv', events_path)
+objects = generate_data(15, object_attributes, 'csv', objects_path)
+location = generate_data(15, location_attributes, 'csv', location_path)
+evidence = generate_data(15, evidence_attributes, 'csv', evidence_path)
